8760421ba01133988540be673e4bc4962c4df591
Denys Neplokhov <35971873+Semaforje@users.noreply.github.com>
2024-02-29 21:55:23 +0200

    static root

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -130,12 +130,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "static"
 
-STATICFILES_DIRS = [
-     BASE_DIR / "static"
-]
+# STATICFILES_DIRS = [
+#      BASE_DIR / "static"
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
