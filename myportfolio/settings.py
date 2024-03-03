70ea8050c9d2092966135bb6a4030d0b53af02a2
Denys Neplokhov <35971873+Semaforje@users.noreply.github.com>
2024-03-03 17:37:09 +0200

    Changing photo file

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -153,10 +153,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
